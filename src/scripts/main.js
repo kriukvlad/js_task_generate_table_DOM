753bf67df5220fe80ac43c71a0aa6380e649ea07
Vladyslav Kriuk <kriukvlad@gmail.com>
2024-12-17 18:58:56 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,7 +354,20 @@ const people = [
   },
 ];
 
-// eslint-disable-next-line no-console
-console.log(people); // you can remove it
-
 // write your code here
+const dashboard = document.querySelector('.dashboard');
+
+const markup = people
+  .map((person) => {
+    return `<tr>
+      <td>${person.name}</td>
+      <td>${person.sex}</td>
+      <td>${person.born}</td>
+      <td>${person.died}</td>
+      <td>${person.died - person.born}</td>
+      <td>${Math.ceil(person.died / 100)}</td>
+    </tr>`;
+  })
+  .join('');
+
+dashboard.innerHTML = markup;
